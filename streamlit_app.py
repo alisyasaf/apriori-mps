854902f873d981a3198e27e8ef8f3d6d5c37ae9a
Alisya Rahma Safitra <117332257+alisyasaf@users.noreply.github.com>
2024-02-28 21:48:41 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from mlxtend.frequent_patterns import apriori, association_rules
+from openpyxl import load_workbook
 
 st.title("Analisis Pola Pembelian Pelanggan Menggunakan Algoritma Apriori (Studi Kasus di Mixue Pasar Sleman)")
 
